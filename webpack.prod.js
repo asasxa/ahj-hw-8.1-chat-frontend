49e0b50a6e0dadfb4b32c40943c2df239eeae8ff
Ruslan Suleimanov <herwer.cool@gmail.com>
2026-01-25 11:52:58 +0300

    i

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
 const TerserPlugin = require('terser-webpack-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -9,8 +9,7 @@ module.exports = merge(common, {
     minimize: true,
     minimizer: [
       new TerserPlugin({}),
-      new OptimizeCSSAssetsPlugin({}),
-      '...',
+      new CssMinimizerPlugin(),
     ],
   },
 });
